de17b02399e2888e9c21b0326c6423300c682092
Arthur Cantarela <cantarela.arthur@gmail.com>
2023-11-16 09:59:59 -0300

    debugging arduino feature flag

--- Tfc/FeatureFlags.h
+++ Tfc/FeatureFlags.h
@@ -1,1 +1,3 @@
+#ifndef FEATURE_ARDUINO
 #define FEATURE_ARDUINO 1
+#endif
--- Tfc/Tfc.ino
+++ Tfc/Tfc.ino
@@ -1,15 +1,16 @@
-//#include "Game.h"
+#include "FeatureFlags.h"
+#include "Game.h"
 
-//Game *game;
+Game *game;
 void setup()
 {
     Serial.begin(9600);
     Serial.println("hii1");
-//    game = new Game();
+    game = new Game();
 }
 
 void loop()
 {
     Serial.println("hii");
-//    game->run();
+    game->run();
 }
